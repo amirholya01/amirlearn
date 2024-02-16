422118cac7ff7c20e4280fd21ab456091a8107ac
amirholya01 <120662275+amirhosseinolya@users.noreply.github.com>
2024-02-16 09:21:41 +0100

    first commit

---
+++ src/main/java/com/amirlearn/amirlearn/AmirlearnApplication.java
@@ -0,0 +1,13 @@
+package com.amirlearn.amirlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmirlearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmirlearnApplication.class, args);
+    }
+
+}
